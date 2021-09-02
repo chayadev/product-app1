8bfee9ae7e868c65f27e452e906486c314f12732
chayadev <yellamarajuchayaa@gmail.com>
2021-09-02 09:08:21 +0530

    firstcommit

---
+++ src/main/java/com/myapp/spring/api/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package com.myapp.spring.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends Exception {
+
+	public ResourceNotFoundException(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
+	
+}
